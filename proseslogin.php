6002422e2d875365333507a3b53f4931c5fb05c6
MoxRha <moxrhaA4@gmail.com>
2023-05-12 16:34:27 +0700

    [Refactor] Perbaikan untuk fitur session login

--- proseslogin.php
+++ proseslogin.php
@@ -3,9 +3,7 @@ session_start();
 include "koneksi.php";
 
 $username = $_POST['username'];
-// $password = base64_encode($_POST['password']);
 $password = $_POST['password'];
-
 
 $session_id = session_id();
 $cek_session = mysqli_query($mysqli, "select * from user where session_id = '$session_id'");
@@ -22,19 +20,21 @@ if (mysqli_num_rows($cek_session) > 0) {
 $cek = mysqli_query($mysqli, "select * from user where username='$username' and password='$password' ");
 if (mysqli_num_rows($cek) > 0) {
     $data = mysqli_fetch_array($cek);
-    $_SESSION['username'] = $data['username'];
-    $_SESSION['status'] = $data['status'];
-    $_SESSION['iduser'] = $data['iduser'];
-    $_SESSION['namauser'] = $data['namauser'];
+    if ($data['session_id'] == '' || $data['session_id'] == $session_id) {
+        // set session data and update session ID in database
+        $_SESSION['username'] = $data['username'];
+        $_SESSION['status'] = $data['status'];
+        $_SESSION['iduser'] = $data['iduser'];
+        $_SESSION['namauser'] = $data['namauser'];
     
-    $session_id = session_id();
-    $iduser = $data['iduser'];
-    mysqli_query($mysqli, "UPDATE user SET session_id='$session_id' WHERE iduser='$iduser' ");
+        $iduser = $data['iduser'];
+        mysqli_query($mysqli, "UPDATE user SET session_id='$session_id' WHERE iduser='$iduser' ");
 
-    echo "<script>window.location.href='index.php?home'</script>";
+        echo "<script>window.location.href='index.php?home'</script>";
+    } else {
+        // session ID mismatch, do not allow login
+        echo "<script>alert('Akun anda sedang login');window.history.go(-1);</script>";
+    }
 } else {
     echo "<script>alert('periksa kembali username dan password yang anda masukan');window.history.go(-1);</script>";
 }
-
-
-
